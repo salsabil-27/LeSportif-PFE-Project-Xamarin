93dd4b0fc51e3e304d9e7e21d0a53c690fc25b46
salsabil zaabar <zaabarsalsabil@gmail.com>
2022-03-27 22:57:56 +0200

    final changes

--- LeSportif/LeSportif.Android/MainActivity.cs
+++ LeSportif/LeSportif.Android/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 using Xamarin.Forms;
+using System.Net.NetworkInformation;
 
 namespace LeSportif.Droid
 {
@@ -15,13 +16,15 @@ namespace LeSportif.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            
+
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
--- LeSportif/LeSportif/Pages/GymPage.xaml.cs
+++ LeSportif/LeSportif/Pages/GymPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
 
 namespace LeSportif.Pages
 {
@@ -16,5 +18,44 @@ namespace LeSportif.Pages
         {
             InitializeComponent();
         }
+
+        public string Legs;
+        public string Shoulder;
+        public string M;
+        
+
+        private async void Scan_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            
+            await Navigation.PushModalAsync(scan);
+
+            scan.OnScanResult += (result) =>
+            {
+
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopModalAsync();
+                    await DisplayAlert("This Machine is for:", "" + result.Text, "Go to Video");
+                    await Navigation.PushAsync(new EquipementPage());
+
+                });
+
+
+            };
+        }
+
+    
+       // public void HandleResult(ZXing.Result rawResult)
+        //{ ProcessResult(rawResult.Text); }
+        //private async void ProcessResult(string text)
+       // {
+            //if (text.StartsWith("legs"))
+            //{
+               // await Navigation.PushAsync(new LegsPage());
+           // }
+       // }
+
+
     }
 }
